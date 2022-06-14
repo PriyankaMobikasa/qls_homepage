6299d2a4b75e28c38847bbd3d6d01b94ebf448dd
Mobikasa-CPU33 <priyankasandokar@mobikasa.com>
2022-06-14 11:54:36 +0530

    mid banner changes

--- src/Homepage/HomepageFiles/Homepage.jsx
+++ src/Homepage/HomepageFiles/Homepage.jsx
@@ -3,12 +3,14 @@ import Banner from '../Banner/Banner';
 import Resources from '../Resources/Resources';
 import './Homepage.css';
 import Services from '../Services/Services';
+import MidBanner from '../MidBanner/MidBanner';
 
 const Homepage = () => {
     return (
         <div>
             <Banner />
             <Services />
+            <MidBanner />
             <Resources />
         </div>
     )
